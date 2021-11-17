59f1218f8bf9876796026661e1bfee1b865e5479
goosy <goosy.jo@gmail.com>
2021-11-17 08:57:26 +0800

    fix str_padding bug

--- src/str_padding.js
+++ src/str_padding.js
@@ -1,9 +1,24 @@
+/**
+ * 将item左侧用占位符填充至指定长度
+ * @date 2021-11-17
+ * @param {number|string} item
+ * @param {number} length
+ * @param {string} placeholder=''
+ * @returns {string}
+ */
 export function str_padding_left(item, length, placeholder = ' ') {
-    const str = Array(length).join(placeholder) + item;
+    const str = Array(length).join(placeholder) + placeholder + item;
     return str.slice(-length);
 }
+/**
+ * 将item右侧用占位符填充至指定长度
+ * @date 2021-11-17
+ * @param {number|string} item
+ * @param {number} length
+ * @param {string} placeholder=''
+ * @returns {string}
+ */
 export function str_padding_right(item, length, placeholder = ' ') {
-    const str = item + Array(length).join(placeholder);
+    const str = item + placeholder + Array(length).join(placeholder);
     return str.slice(0, length);
 }
-
